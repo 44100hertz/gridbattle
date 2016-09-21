20d59db0be5b3107787513ab5249d94a8bbb8918
44100hertz <ssaammp@gmail.com>
2016-09-21 09:32:26 -0500

    made staling state -1

--- vslice/input.lua
+++ vslice/input.lua
@@ -1,4 +1,4 @@
-local stale = 1000
+local stale = -1
 
 local keyBind = {
    a ="x", b="z", l="a", r="s", start="enter", sel="shift",
@@ -10,15 +10,20 @@ local buttons = {
 }
 
 input = {
+   --[[ Scan for inputs
+   ]]--
    update = function ()
       for k,v in pairs(buttons) do
 	 local pressed = love.keyboard.isScancodeDown(keyBind[k])
-	 if pressed and buttons[k]~=stale then buttons[k]=buttons[k]+1
+	 if pressed and buttons[k]>stale then buttons[k]=buttons[k]+1
 	 else buttons[k]=0
 	 end
       end
    end,
-   
+
+   --[[ Make a specific input go "stale" so that it will not
+      be registered until let go and re-pressed
+   ]]--
    stale = function (arg)
       arg = arg or "move"
       if arg=="move" then
@@ -31,6 +36,9 @@ input = {
       end
    end,
 
+   --[[ Grab an input value from the table
+      BufferLen: maximum time ago button was pressed
+   --]]
    check = function (button, bufferLen)
       bufferLen = bufferLen or 6
       if buttons[button]>0 and buttons[button]<bufferLen then return true
