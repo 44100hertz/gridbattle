368e8dce983b751842f36aab90e19924bf162573
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-10-13 14:41:36 -0500

    Revised Sheet for clarity

--- src/Sheet.lua
+++ src/Sheet.lua
@@ -5,7 +5,7 @@ reads data from sheet data file
 currently returns a love.graphics.draw instruction for that given sheet
 
 sheet_data format:
-sheet = {
+{
    size = {x=_, y=_}       ; default size in pixels
    img = _                 ; image filename to make sheet from. Can be overridden.
    {                       ; start of a strip
@@ -34,31 +34,31 @@ local function read_strip(offset, size, img_size, num)
    return quads
 end
 
---[[ Create a new sheet based on sheet_data
+--[[ Create a new sheet based on data
    'img' should be a love.graphics.image, is optional, and overrides the sheet
    returns a table of named strips, with numbered love quads
---]] 
-function Sheet.new(sheet, img)
-   local o = {}
-   o.img = img or assert(love.graphics.newImage(sheet.file))
+--]]
+function Sheet.new(data, img)
+   local sheet = {}
+   sheet.img = img or assert(love.graphics.newImage(data.file))
 
    -- Parameters outside "strip"
    local img_size = {}
-   img_size.x, img_size.y = o.img:getDimensions()
-   local default_size = sheet.size
+   img_size.x, img_size.y = sheet.img:getDimensions()
+   local default_size = data.size
 
    -- Read each strip
-   for k,_ in pairs(sheet.strips) do
-      local size = sheet.strips[k].size or default_size
-      o[k] = read_strip(
-	 sheet.strips[k].pos,
+   for k,_ in pairs(data.strips) do
+      local size = data.strips[k].size or default_size
+      sheet[k] = read_strip(
+	 data.strips[k].pos,
 	 size,
 	 img_size,
-	 sheet.strips[k].num
+	 data.strips[k].num
       )
    end
    
-   return o
+   return sheet
 end
 
 return Sheet
