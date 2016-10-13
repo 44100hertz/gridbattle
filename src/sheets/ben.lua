891d1409cde51d1f70e492b09602fb70b6c4bc52
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-10-12 22:59:56 -0500

    Added sheet data for ben and stage

---
+++ src/sheets/ben.lua
@@ -0,0 +1,18 @@
+return {
+   size = {x=50, y=60},
+   file = "img/ben.png",
+   strips = {
+      idle = {
+	 pos = {x=0, y=0},
+	 num = 2,
+      },
+      move = {
+	 pos = {x=0, y=60},
+	 num = 2,
+      },
+      shoot = {
+	 pos = {x=0, y=120},
+	 num = 1,
+      }
+   }
+}
---
+++ src/sheets/stage.lua
@@ -0,0 +1,14 @@
+return {
+   size = {x=40, y=32},
+   file = "img/stage.png",
+   strips = {
+      left = {
+	 pos = {x=0,y=0},
+	 num = 2,
+      },
+      right = {
+	 pos = {x=0,y=32},
+	 num = 2,
+      }
+   }
+}
