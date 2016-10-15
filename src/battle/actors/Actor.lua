5fb5e0fcc10e23842577e7ff97a2865aa8347202
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-10-15 11:26:07 -0500

    made Actor do more; new state machine model

--- src/battle/actors/Actor.lua
+++ src/battle/actors/Actor.lua
@@ -1,58 +1,100 @@
 --[[ Actor
-A set of extensible, common Actor functions
-to be included LOCALLY in Actor/ classes
+A stage actor state machine and animation system
+
+machine functions:
+   start()  ; reset to a neutral state, e.g. idle
+   update() ; run one frame
+   draw()   ; draw current state, does not move anything forward
+   act()    ; do something based on AI, RNG, user input, etc.
+
+format:
+   self.sheet         ; Required. Where to draw from.
+   self.frame         ; [internal] current frame index
+   self.state_time    ; [internal] how far into current animation
+   self.frame_time    ; [internal] how far into current strip frame
+   self.state = {
+       anim,         ; How to draw/time this state. see animation format below
+       now,          ; [optional] function for current action. nil = [do nothing]
+       after,        ; [optional] function for next action. nil = start.
+   }
+   self.anim = {
+       strip = _,               ; what strip to play; sheet.x, etc.
+       timing = {_,_,...},      ; length of each frame
+       order = {_,_,...},       ; [optional] what order to play frames in
+       length = _,              ; [optional] when to enter next state. nil = loop
+       iasa = _,                ; [optional] frames until actor can act again
+   }
 --]]
 
 Actor = {}
 
--- Start or re-start an Actor without reloading its module
--- initialize ALL module state
--- do this instead of just putting things in the package header
-function Actor:new(space, side, speed)
-   local o = {}
+function Actor:new (o)
+   o = o or {}
    setmetatable(o, self)
    self.__index = self
    
-   -- init values
-   self.space = space or {x=2,y=2} -- Actor starting position
-   self.side  = side  or "left"    -- Actor starting stage side
-   self.speed = speed or 1         -- State speed
-   
-   -- init actions
+   -- Init values
+   self.space = {x=2,y=2} -- Actor starting position
+   self.side  = "left"    -- Actor starting stage side
+   self.speed = 1         -- State speed
+
+   -- Init actions
    stage.occupy(self.space)         -- Create collision on space
    self.pos = stage.pos(self.space) -- Find the actual coords for drawing
 
-   -- now using a Sheet; soon to use an Animation
-   self.sheet = Sheet.new(require "sheets/ben")
+   -- TODO: where to put origin data? In sheet?
    self.origin = {x=25,y=57}
 
    -- Init state machine
-   self.state = self.init
-   self.state_timer = 0
+   self.state = { now = self.start }
+   self.frame = 0
+   self.state_time = 0
+   self.frame_time = 0
 
    return o
 end
 
--- Assumed that this is done before each draw
-function Actor:update(dt)
-   self:state()
-   self.state_timer = self.state_timer + self.speed*60*dt
-end
+--[[
+   Actor:update
+   Done before each draw
+--]]
+function Actor:update (dt)
+   -- March time forward
+   self.state_time = self.state_time + self.speed*60*dt
+   self.frame_time = self.frame_time + self.speed*60*dt
 
--- Generic draw function, will be replaced
-function Actor:draw()
-   love.graphics.draw(self.sheet.img, self.frame, self.pos.x, self.pos.y,
-		      0, 1, 1, self.origin.x, self.origin.y)
-end
+   -- Run current state
+   if self.state.now then self.state:now() end
 
--- For the end of animations; will be replaced
-function Actor:cooldown()
-   if self.state_timer > 10 then
-      return self:init()
+   -- Go through animations
+   local length = self.state.anim.length
+   local iasa = self.state.anim.iasa
+   if length and not iasa then iasa = length-1 end
+   -- If animation ended
+   if length and self.state_time > length then
+      self.state = self.after or self.start
+   end
+   -- if animation interruptible
+   if not iasa or self.state_time > iasa then
+      self.act()
+   end
+
+   -- Go through frames
+   local timing = self.anim.timing[frame]
+   if self.frame_time > timing then
+      self.frame_time = self.frame_time - timing
+      self.frame = self.frame + 1
    end
 end
 
--- Placeholder function; do nothing!
-function Actor:init() end
+function Actor:draw ()
+   love.graphics.draw(
+      self.sheet.img,
+      self.anim.strip[frame],
+      self.pos.x, self.pos.y,
+      0, 1, 1,
+      self.origin.x, self.origin.y
+   )
+end
 
 return Actor
