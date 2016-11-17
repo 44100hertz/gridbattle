55a7dc3d3a88a5f53f73f21c3155ca1fd84ab49d
Sam Pagenkopf <ssaammp@gmail.com>
2016-11-17 17:10:15 -0600

    cleared out hacky code. Added "iasa" behavior

--- src/battle/actors/Player.lua
+++ src/battle/actors/Player.lua
@@ -11,11 +11,13 @@ local anim = {
 
 local idle = function (self)
    self.statetime = 0
+   self.iasa = 0
    self.anim = anim.idle
 end
 
 local shoot = function (self)
    self.statetime = 0
+   self.iasa = 10
    self.anim = anim.shoot
    self.actionable = false
 end
@@ -23,19 +25,20 @@ end
 local move = function (self, dx, dy)
    self.statetime = 0
    self.anim = anim.move
-   self.dx, self.dy = dx, dy
-   self.actionable = false
+   self.iasa = 20
+   self.x = self.x + dx
+   self.y = self.y + dy
 end
 
 return {
    start = function (self)
-      self.actionable = true
+      self.iasa = 0 -- actionability
       self.dx, self.dy = 0,0
       idle(self)
    end,
 
    update = function (self)
-      if self.actionable then
+      if self.iasa==0 then
 	 if     input.a>0   then shoot(self)
 	 elseif input.du>0  then move(self, 0, -1)
 	 elseif input.dd>0  then move(self, 0, 1)
@@ -44,30 +47,12 @@ return {
 	 end
       end
 
-      local function sign(num)
-	 if num > 0 then return 1
-	 elseif num == 0 then return 0
-	 elseif num < 0 then return -1
-	 end
-      end
-
-      if self.dx~=0 or self.dy~=0 then
-	 self.x = self.x + sign(self.dx)/32
-	 self.y = self.y + sign(self.dy)/32
-	 self.dx = self.dx - sign(self.dx)/32
-	 self.dy = self.dy - sign(self.dy)/32
-	 if self.dx==0 and self.dy==0 then
-	    self.actionable = true
-	    idle(self)
-	 end
-      end
-
-      -- Animation-based logic --
-      self.statetime = self.statetime + self.anim.speed
+      self.statetime = self.statetime + 1
+      if self.iasa>0 then self.iasa = self.iasa-1 end
    end,
 
    draw = function (self, x, y)
-      local frameindex = math.floor(self.statetime) % self.anim.length
+      local frameindex = math.floor(self.statetime * self.anim.speed) % self.anim.length
       love.graphics.draw(img, sheet[frameindex + 1],
 			 x, y, 0, 1, 1, 25, 57)
    end,
