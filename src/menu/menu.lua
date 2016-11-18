09943b5d20d3060e4decce15365df36e0998008d
Sam Pagenkopf <ssaammp@gmail.com>
2016-11-18 15:27:42 -0600

    submenu

--- src/menu/menu.lua
+++ src/menu/menu.lua
@@ -1,4 +1,8 @@
-local rootmenu = {
+local rootmenu, optionsmenu
+
+local currentmenu, sel
+
+rootmenu = {
    {text = "start demo",
     func = function ()
        main.loadstate(require "battle/battle")
@@ -9,17 +13,29 @@ local rootmenu = {
        love.event.quit()
     end
    },
+   {text = "options",
+    func = function ()
+       current = optionsmenu
+       sel = 1
+    end
+   },
+}
+
+optionsmenu = {
+   {text = "back",
+    func = function ()
+       current = rootmenu
+    end
+   }
 }
 
 local bg = love.graphics.newImage("img/menu.png")
 
-local currentmenu
-local sel = 1
 local framecounter
-
 return {
    init = function ()
       love.graphics.setFont(fonts.std15)
+      sel = 1
       current = rootmenu
       counter = 0
    end,
@@ -40,11 +56,11 @@ return {
 
    draw = function ()
       love.graphics.draw(bg)
+      counter = counter + 1
+      local size = (counter/3 % 5) + 1
       for k,v in ipairs(current) do
-	 counter = counter + 1
-	 local size = (counter/10 % 5) + 1
-	 love.graphics.circle("fill", 70, 87+sel*20, size)
-	 love.graphics.print(v.text, 80, 80+k*20)
+	 love.graphics.circle("fill", 70, 67+sel*20, size)
+	 love.graphics.print(v.text, 80, 60+k*20)
       end
    end,
 }
