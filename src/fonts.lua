4c761ace3dc082eed96be26ee8af82459bfb17e7
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-11-19 01:46:46 -0600

    Added tiny font

--- src/fonts.lua
+++ src/fonts.lua
@@ -1,5 +1,7 @@
-local asciistring = " abcdefghijklmnopqrstuvwxyz"
+local letters = "abcdefghijklmnopqrstuvwxyz"
+local numbers = "1234567890"
 
 return {
-   std15 = love.graphics.newImageFont("img/font.png", asciistring)
+      std15 = love.graphics.newImageFont("img/font.png", " " .. letters),
+      tiny = love.graphics.newImageFont("img/font-tiny.png", " " .. letters .. numbers),
 }
