908f2cc0a5e54c7b77263f3d8bc940647e2690e2
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-11-22 15:59:35 -0600

    moved loadset back into data

--- src/battle/data.lua
+++ src/battle/data.lua
@@ -2,13 +2,47 @@
    All data for a given battle. NOT to be used by any actor.
 --]]
 
+local stage = {
+   numx=6, numy=3, -- number of platforms
+   x=-24, y=74, -- -24 = -32 + 8 (dist from screen edge)
+   w=64, h=40, -- size of each platform
+   -- stored by loadset: 2d array stage[x][y] such that x and y
+   -- are the position of a given platform
+}
+
+local actors = {}
+
 return {
-   stage = {
-      numx=6, numy=3, -- number of platforms
-      x=-24, y=74, -- -24 = -32 + 8 (dist from screen edge)
-      w=64, h=40, -- size of each platform
-      -- stored by loadset: 2d array stage[x][y] such that x and y
-      -- are the position of a given platform
-   },
-   actors = {},
+   loadset = function (set)
+      actors = {}
+
+      -- Stage panels
+      local turf = set.stage.turf
+      local panel = require "battle/actors/panel"
+      for x = 1,stage.numx do
+         stage[x] = {}
+         for y = 1,stage.numy do
+            local newpanel = {
+               class=panel,
+               x=x, y=y, z=-8,
+               side = (x <= turf[y]) and "left" or "right"
+            }
+            stage[x][y] = newpanel
+            battle.addactor(newpanel)
+         end
+      end
+
+      -- Player
+      local player = {
+         class=require "battle/actors/player",
+         x=set.stage.spawn.x, y=set.stage.spawn.y, side="left"
+      }
+      battle.addactor(player)
+
+      -- Any actors specified for level; enemies
+      for _,v in ipairs(set.actors) do battle.addactor(v) end
+   end,
+
+   stage = stage,
+   actors = actors,
 }
