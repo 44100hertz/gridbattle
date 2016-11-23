6b08a29a1bfbc372d51d7d9d8c21f898f7093254
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-11-22 18:29:11 -0600

    map-based menu

--- src/menu/title.lua
+++ src/menu/title.lua
@@ -1,73 +1,65 @@
-local main = require "main"
-local input = require "input"
 local fonts = require "fonts"
+local input = require "input"
+local main = require "main"
 
-local rootmenu, optionsmenu
+local drawtxt = function (text, x, y)
+end
 
-local current, sel
-local selstack = {}
-
-rootmenu = {
-   {text = "start demo",
-    func = function ()
-       main.pushstate(require "battle/main", require "battle/sets/test")
-    end
-   },
-   {text = "exit",
-    func = function ()
-       love.event.quit()
-    end
-   },
-   {text = "options",
-    func = function ()
-       current = optionsmenu
-       table.insert(selstack, sel)
-       sel = 1
-    end
-   },
+local options
+local root = {}
+root[1] = {
+   x=120, y=80, text="start",
+   u = root[3], d = root[2],
+   a = function ()
+      main.pushstate(require "battle/main", require "battle/sets/test")
+   end,
+}
+root[2] = {
+   x=120, y=100, text="options",
+   u = root[1], d = root[3],
+   a = function () return optionsmenu end,
+}
+root[3] = {
+   x=120, y=120, text="exit",
+   u = root[2], d = root[1],
+   a = function () love.event.quit() end,
+   draw = function () drawtxt("exit", 120, 100) end,
 }
 
-optionsmenu = {
-   {text = "back",
-    func = function ()
-       current = rootmenu
-       sel = table.remove(selstack)
-    end
-   }
-}
+-- options = {}
+-- options[1] = {
+--    draw = function () drawtxt("back", 120, 60) end,
+--    choose = function () return 0 end
+-- }
 
 local bg = love.graphics.newImage("img/menu.png")
+local current, sel
 
-local counter
 return {
    start = function ()
-      sel = 1
-      current = rootmenu
-      counter = 0
+      current = root
+      sel = current[1]
    end,
 
    update = function ()
-      if input.dd == 1 then
-	 sel = sel % (#current) + 1
-      end
-
-      if input.du == 1 then
-	 sel = (sel-2) % (#current) + 1
-      end
-
-      if input.a == 1 or input.start == 2 then
-	 current[sel].func()
+      if input.du == 1 and sel.u then
+	 sel = sel.u
+      elseif input.dd == 1 and sel.d then
+         sel = sel.d
+      elseif input.dl == 1 and sel.l then
+         sel = sel.l
+      elseif input.dr == 1 and sel.r then
+         sel = sel.r
+      elseif input.a == 1 and sel.a then
+	 sel.a()
       end
    end,
 
    draw = function ()
       love.graphics.draw(bg)
-      counter = counter + 1
-      local size = (counter/3 % 5) + 1
-      love.graphics.circle("fill", 110, 67+sel*20, size)
-      love.graphics.setFont(fonts.std15)
-      for k,v in ipairs(current) do
-	 love.graphics.print(v.text, 120, 60+k*20)
+      for _,v in ipairs(current) do
+         love.graphics.setFont(fonts.std15)
+         love.graphics.print(v.text, v.x, v.y)
       end
    end,
 }
