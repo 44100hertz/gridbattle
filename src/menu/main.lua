173dab99ee5fc555bfadbc392bb224b8d2aee839
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2016-11-23 13:33:16 -0600

    flexible menu code

--- src/menu/main.lua
+++ src/menu/main.lua
@@ -3,26 +3,51 @@ local fonts = require "fonts"
 
 local mod, current, sel
 local bg
+local wait, reprate
+
+local directions = {
+   du = "u", dd = "d",
+   dl = "l", dr = "r",
+}
+
+local buttons = {
+   "a", "b", "l", "r", "start", "sel"
+}
 
 return {
-   start = function (lastmod, root, newbg)
+   start = function (lastmod, newmenu, newbg)
+      menu = newmenu
+      sel = menu[1]
       mod = lastmod
-      current = root
-      sel = current[1]
       bg = newbg
+      wait = 1
+      reprate = 20
    end,
 
    update = function ()
-      if input.du == 1 and sel.u then
-	 sel = current[sel.u]
-      elseif input.dd == 1 and sel.d then
-         sel = current[sel.d]
-      elseif input.dl == 1 and sel.l then
-         sel = current[sel.l]
-      elseif input.dr == 1 and sel.r then
-         sel = current[sel.r]
-      elseif input.a == 1 and sel.a then
-	 sel.a()
+      local isdirpressed = false
+      for k,v in pairs(directions) do
+         if input[k] > 0 then isdirpressed = true end
+         if input[k] == wait and sel[v] then
+            wait = wait==1 and 20 or wait+8
+            sel = menu[sel[v]]
+            break
+         end
+      end
+      if not isdirpressed then
+         wait = 1
+      end
+
+      for _,v in ipairs(buttons) do
+         if input[v]==1 then
+            if sel[v] then
+               sel[v]()
+               break
+            elseif menu[v] then
+               menu[v]()
+               break
+            end
+         end
       end
    end,
 
@@ -30,7 +55,7 @@ return {
       if mod then mod.draw() end
       if bg then love.graphics.draw(bg) end
       love.graphics.circle("fill", sel.x-20, sel.y, 8)
-      for _,v in ipairs(current) do
+      for _,v in ipairs(menu) do
          love.graphics.setFont(fonts.std15)
          love.graphics.print(v.text, v.x, v.y)
       end
