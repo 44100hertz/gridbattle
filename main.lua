552de02abb633fbd037aba0d84c813d6ea5c8cee
44100hertz <ssaammp@gmail.com>
2016-11-25 13:22:58 -0600

    got rid of hacky ffmpeg encode

--- main.lua
+++ main.lua
@@ -7,6 +7,12 @@ gameheight = 240
 gamescale = 2
 
 love.run = function ()
+   local outdir
+   if arg[2] == "dump" then
+      outdir = "/out" .. os.time()
+      love.filesystem.createDirectory(outdir)
+   end
+
    love.math.setRandomSeed(os.time())
    local time = 0
 
@@ -40,17 +46,10 @@ love.run = function ()
       love.graphics.draw( canvas, 0,0,0, gamescale )
       love.graphics.present()
 
-      if arg[2] == "dump" then
+      if outdir then
          time = time + 1
-         local screenshot = love.graphics.newScreenshot()
-         local name = string.format("%3.3d.tga", time)
-         screenshot:encode("tga", name)
+         local imgdata = canvas:newImageData()
+         imgdata:encode("tga", outdir .. "/" .. time .. ".tga")
       end
-   end
-end
-
-love.quit = function ()
-   if arg[2] == "dump" then
-      os.execute("ffmpeg -loglevel warning -framerate 60 -i ~/.local/share/love/src/%03d.tga -vf scale=iw*2:ih*2:sws_flags=neighbor -c:v vp8 -qmin 20 -qmax 20 out/vid.webm")
    end
 end
