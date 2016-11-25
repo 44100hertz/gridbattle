4522270373e10f0b0b69c905ea38835c8616b5fa
44100hertz <ssaammp@gmail.com>
2016-11-25 14:17:32 -0600

    general actor functions

--- res/battle/actors/player.lua
+++ res/battle/actors/player.lua
@@ -1,67 +1,69 @@
 local anim = require "src/anim"
 local input = require "src/input"
+
+local actor = require "src/battle/actor"
 local battle = require "src/battle/battle"
 
-local img = love.graphics.newImage("res/battle/actors/ben.png")
-local iw, ih = img:getDimensions()
-
-local sheet = anim.sheet(0, 0, 50, 60, iw, ih, 2, 3)
-
-local states = {
-   idle = {
-      anim = {1, speed=0},
-      iasa = 0,
-   },
-   move = {
-      anim = {3,4, speed=0.2},
-      iasa = 12,
-      length = 22,
-      update = function (self)
-         if self.time == 10 then
-            self.x, self.y = self.goalx, self.goaly
-         end
-      end,
-   },
-   shoot = {
-      anim = {5,6, speed=0.05},
-      iasa = 25,
-      length = 30,
-      update = function (self)
-         if self.time == 10 then
-            battle.addactor(
-               {class=require "res/battle/actors/bullet",
-		x=self.x+0.3, y=self.y, z=40}
-            )
-         end
-      end
-   }
+local states = {}
+states.idle = {
+   anim = {1, speed=0},
+   iasa = 0,
 }
-
-local loadstate = function (self, state)
-   self.state = state
-   self.time = 0
-end
+states.move = {
+   anim = {3,4, speed=0.2},
+   iasa = 12,
+   length = 22,
+   update = function (self)
+      if self.time == 10 then
+	 self.x, self.y = self.goalx, self.goaly
+      end
+   end,
+   finish = states.idle,
+}
+states.shoot = {
+   anim = {5,6, speed=0.05},
+   iasa = 25,
+   length = 30,
+   update = function (self)
+      if self.time == 10 then
+	 battle.addactor(
+	    {class=require "res/battle/actors/bullet",
+	     x=self.x+0.3, y=self.y, z=40}
+	 )
+      end
+   end,
+   finish = states.idle,
+}
 
 local move = function  (self, dx, dy)
    local goalx, goaly = self.x+dx, self.y+dy
    if battle.occupy(self, goalx, goaly, "left") then
       self.goalx, self.goaly = goalx, goaly
       battle.free(self.x, self.y)
-      loadstate(self, states.move)
+      actor.loadstate(self, states.move)
    end
+end
+
+local img = love.graphics.newImage("res/battle/actors/ben.png")
+local sheet
+do
+   local iw, ih = img:getDimensions()
+   sheet = anim.sheet(0, 0, 50, 60, iw, ih, 2, 3)
 end
 
 return {
    height=52,
    start = function (self)
+      self.img = img
+      self.sheet = sheet
       self.stand = true
       battle.occupy(self, self.x, self.y)
-      loadstate(self, states.idle)
+      actor.loadstate(self, states.idle)
    end,
 
    update = function (self)
       if self.time >= self.state.iasa then
-         if input.a > 0 then loadstate(self, states.shoot) end
+         if input.a > 0 then actor.loadstate(self, states.shoot) end
          if input.du>0  then move(self, 0, -1)
          elseif input.dd>0  then move(self, 0, 1)
          elseif input.dl>0  then move(self, -1, 0)
@@ -69,19 +71,10 @@ return {
          end
       end
 
-      if self.time == self.state.length then
-         loadstate(self, states.idle)
-      end
-
-      if self.state.update then self.state.update(self) end
-      self.time = self.time + 1
+      actor.stateupdate(self)
    end,
 
    draw = function (self, x, y)
-      local frameindex =
-         math.floor(self.time * self.state.anim.speed)
-         % #self.state.anim
-      local frame = sheet[self.state.anim[frameindex + 1]]
-      love.graphics.draw(img, frame, x, y, 0, 1, 1, 25, 5)
+      actor.drawanimated(self, x, y)
    end,
 }
---
+++ src/battle/actor.lua
@@ -0,0 +1,25 @@
+local loadstate = function (self, state)
+   self.state = state
+   self.time = 0
+end
+
+return {
+   loadstate = loadstate,
+
+   stateupdate = function (self)
+      if self.time == self.state.length then
+         loadstate(self, self.state.finish)
+      end
+
+      if self.state.update then self.state.update(self) end
+      self.time = self.time + 1
+   end,
+   
+   drawanimated = function (self, x, y)
+      local frameindex =
+         math.floor(self.time * self.state.anim.speed)
+         % #self.state.anim
+      local frame = self.sheet[self.state.anim[frameindex + 1]]
+      love.graphics.draw(self.img, frame, x, y, 0, 1, 1, 25, 5)
+   end
+}
