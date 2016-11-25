406c924d53a27426c9865f56a41457c97886667e
44100hertz <ssaammp@gmail.com>
2016-11-25 13:03:38 -0600

    some joypad code

--- src/input.lua
+++ src/input.lua
@@ -1,12 +1,33 @@
 -- keys should be "friendly" names
 -- values must be valid scancodes
 local keyBind = {
-   a ="x", b="z",
+   a="x", b="z",
    l="a", r="s",
    st="return", sel="rshift",
    du="i", dd="k",
    dl="j", dr="l"
 }
+
+local joyBind = {
+   a="a", b="b",
+   l="leftshoulder", r="rightshoulder",
+   st="start", sel="back",
+   du="dpup", dd="dpdown",
+   dl="dpleft", dr="dpright",
+}
+
+local joy = love.joystick.getJoysticks()[1]
+
+local joy2hat = function (lr, ud)
+   local dz = 0.5 --deadzone
+   if math.abs(lr) > math.abs(ud) then -- left/right
+      if lr < -dz then return "dl" end
+      if lr > dz  then return "dr" end
+   else
+      if ud < -dz then return "du" end
+      if ud > dz  then return "dd" end
+   end
+end
 
 -- populate an array of buttons
 local buttons = {}
@@ -14,8 +35,18 @@ for k,_ in pairs(keyBind) do buttons[k] = 0 end
 
 local input = {
    update = function ()
+      local fakehat
+      if joy then
+	 fakehat = joy2hat(joy:getAxis(1),joy:getAxis(2))
+      else
+	 fakehat = 0
+      end
+
       for k,v in pairs(keyBind) do
-	 if love.keyboard.isScancodeDown(v) then
+	 if love.keyboard.isScancodeDown(v) or
+	    joy and joy:isGamepadDown(joyBind[k]) or
+	    fakehat == k
+	 then
 	    if buttons[k] > -1 then
 	       buttons[k] = buttons[k]+1
 	    end
