92344172af074490548d55b86191e0255f222d81
Sam Pagenkopf <ssaammp@gmail.com>
2017-02-25 01:50:22 -0600

    more stuff in example

--- actor-example.lua
+++ actor-example.lua
@@ -17,19 +17,29 @@ states.die = { -- Automatically enter this state on death
 actor = {
    -- Graphical --
    img = "res/img/cool.jpg", -- image from which to make sheet
+   shadow = { -- optional
+      img="res/img/shadow.jpg",
+      ox = 25, oy = 25, -- shadow origin
+   }
    new_sheet =  -- tells the game to load this sheet
       {0,0,50,60,2,2}, -- xoff, yoff, xsize, ysize, numx, numy
-   h=20, -- height; used for stacking, etc.
-   ox=5, oy=5 -- X and Y origin of the sprite; the center of where they stand
+   height = 20, -- used for stacking, etc.
+   ox = 5, oy = 5 -- X and Y origin of the sprite; the center of where they stand
 
    -- Functional --
-   hp=200, -- set if this actor has HP
+   max_hp=200, -- set if this actor has HP.
    hide_hp = nil,
    hbox = 1, -- single-square hitbox
    tangible = true, -- occupy physical space, panel
+   group = "enemy" -- collision group
+   collide = function (self, with) end, -- extra stuff to run on collision
 
-   init = function (self) end, -- when actor is initialized ingame
-   act = function (self) end, -- run during idle state and interruptability
+   -- Generally readonly vars --
+   hp=200,
+
+   start = function (self) end, -- optional; when actor is initialized
+   act = function (self) end, -- optional; run when actionable
+   draw = function (self) end, -- optional; draw extra things
 
    states = states, -- states redefined here so that they can cross-reference
 }
