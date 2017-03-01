7d985d3a61a1dc76f5d302366a0aa43a35b7f9a2
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-01 15:38:55 -0600

    separated chip images

--- res/menu/chips.lua
+++ res/menu/chips.lua
@@ -2,21 +2,30 @@ local anim = require "src/anim"
 local state = require "src/state"
 local input = require "src/input"
 
-local img = love.graphics.newImage("res/battle/chips.png")
-local w,h = img:getDimensions()
+local sheet = {}
 
-local sheet = {
-   bg = anim.sheet(0,0,128,160,1,1,w,h)[1][1],
-   button = anim.sheet(128,0,24,16,2,1,w,h)[1],
-   chip = anim.sheet(128,16,16,16,6,1,w,h)[1],
-   letter = anim.sheet(128,32,16,8,5,1,w,h)[1],
-   art = anim.sheet(0,160,64,120,4,1,w,h)[1],
-}
+local menu = love.graphics.newImage("res/menu/chips.png")
+local w,h = menu:getDimensions()
+sheet.bg = anim.sheet(0,0,128,160,1,1,w,h)[1][1]
+sheet.button = anim.sheet(128,0,24,16,2,1,w,h)[1]
+
+local chips = love.graphics.newImage("res/battle/chips.png")
+local w,h = chips:getDimensions()
+sheet.chip = anim.sheet(0,0,16,16,6,1,w,h)[1]
+sheet.letter = anim.sheet(0,16,16,8,5,1,w,h)[1]
+sheet.art = anim.sheet(0,24,64,120,4,1,w,h)[1]
 
 local lastmod
+local palette, queue
+
 return {
    start = function (new_lastmod)
       lastmod = new_lastmod
+      palette = {}
+      for i=1,5 do
+         palette[i] = math.floor(math.random()*4)+3
+      end
+      queue = {}
    end,
 
    update = function ()
@@ -27,18 +36,21 @@ return {
 
    draw = function ()
       lastmod.draw()
-      love.graphics.draw(img, sheet.bg)
+      love.graphics.draw(menu, sheet.bg)
 
       local x,y
 
       -- Chip line
+      local i=1
       y=104
       for _=1,2 do
          x=8
          for _=1,5 do
-            love.graphics.draw(img, sheet.chip[2], x, y)
-            love.graphics.draw(img, sheet.letter[1], x, y+16)
+            palchip = palette[i] or 2
+            love.graphics.draw(chips, sheet.chip[palchip], x, y)
+            love.graphics.draw(chips, sheet.letter[1], x, y+16)
             x=x+16
+            i=i+1
          end
          y=y+24
       end
@@ -46,7 +58,7 @@ return {
       -- Queue
       x,y = 104,24
       for _=1,5 do
-         love.graphics.draw(img, sheet.chip[2], x, y)
+         love.graphics.draw(chips, sheet.chip[2], x, y)
          y=y+16
       end
    end,
