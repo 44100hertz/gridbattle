a4debcc941cd42457ccdd41ec79ef42ea59a9300
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-01 19:02:00 -0600

    "cards" = "chips"

--- src/Deck.lua
+++ src/Deck.lua
@@ -3,16 +3,16 @@ local Deck = {}
 Deck.__index = Deck
 
 -- Copy static deck data into a deck
-function Deck:new(new_Deck)
-   self = self or {cards={}, index=1}
+function Deck.new(new_Deck)
+   self = {chips={}, index=1}
    setmetatable(self, Deck)
-   for k,v in ipairs(new_Deck) do self.cards[k] = v end
+   for k,v in ipairs(new_Deck) do self.chips[k] = v end
    return self
 end
 
 function Deck:shuffle()
    local shuffled = {}
-   local len = #self.cards
+   local len = #self.chips
    for i=1,len do
       -- Find a random place to put the next card
       index = love.math.random(len)
@@ -21,9 +21,9 @@ function Deck:shuffle()
          index = (index % len) + 1
       end
       -- Place the card there
-      shuffled[index] = self.cards[i]
+      shuffled[index] = self.chips[i]
    end
-   self.cards = shuffled
+   self.chips = shuffled
 end
 
 -- Draw a deck, optionally fill a palette
@@ -31,7 +31,7 @@ function Deck:draw(num, palette)
    palette = palette or {}
    for i=1,num do
       if not palette[i] then
-         draw[i] = self.cards[index]
+         palette[i] = self.chips[index]
          self.index = self.index + 1
       end
    end
