cb59a6bf36857ea5fe04e45a5c388ce4b6fa31e8
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-03 02:13:34 -0600

    wheel crate shows up

--- res/chips/WhlCrate.lua
+++ res/chips/WhlCrate.lua
@@ -1,1 +1,11 @@
-return {}
+local actors = require "src/battle/actors"
+
+return {
+   img="wheel_crate",
+   sheet={0,0,45,60,1,1,45,60},
+   tangible=true,
+   ox=17, oy=35,
+   start = function (self)
+      self.x = self.x + 2
+   end,
+}
