34b3b2446d2d6c93c17b147cb0a5161237a58c41
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-02 18:57:19 -0600

    boots...maybe

--- res/chips/Boots.lua
+++ res/chips/Boots.lua
@@ -1,1 +1,14 @@
-return {}
+local actors = require "src/battle/actors"
+
+return {
+   act = function (actor)
+      actors.add(
+         {x=actor.x+0.3, y=actor.y, z=40,
+          img="boots", damage=40,
+          sheet={0,0,24,16,6,1}
+         },
+         "bullet"
+      )
+
+   end
+}
