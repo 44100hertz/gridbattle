ce0b2cf095adf0e568ef15f77861e8a2c002e358
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-02 19:42:53 -0600

    bullet damage,lifespan

--- res/chips/Triangle.lua
+++ res/chips/Triangle.lua
@@ -2,9 +2,10 @@ local actors = require "src/battle/actors"
 
 local Triangle = {
    img = "bullet",
+   damage = 80,
    sheet = {0,0,16,16,6,1},
    collide_die = true,
-   lifespan = 600,
+   lifespan = 60,
    size = 8/64,
    states = {
       idle = {
