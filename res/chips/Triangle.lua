f760d4cfb07c65cd8c903482ea9587c7a7acee5a
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-02 19:28:44 -0600

    put bullet code into Triangle

--- res/chips/Triangle.lua
+++ res/chips/Triangle.lua
@@ -1,12 +1,27 @@
 local actors = require "src/battle/actors"
 
+local Triangle = {
+   img = "bullet",
+   sheet = {0,0,16,16,6,1},
+   collide_die = true,
+   size = 8/64,
+   states = {
+      idle = {
+         row=1, anim = {1,2,3,4,5,6},
+         speed = 5,
+      },
+   },
+   dx = 0.01,
+
+   update = function (self)
+      self.dx = self.dx * 1.1
+      self.x = self.x + self.dx
+      if self.x > 10 then self.despawn = true end
+   end,
+}
+
 return {
    act = function (actor)
-      actors.add(
-         {x=actor.x+0.3, y=actor.y, z=40,
-          img="bullet", damage=40,
-         },
-         "bullet"
-      )
+      actors.add({x=actor.x, y=actor.y, z=40}, Triangle)
    end
 }
