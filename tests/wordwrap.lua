1c630f0c97f00078f6ccb0a053728475ef0bad66
sam <sam@bernie.pres>
2017-03-04 02:05:42 -0600

    sane test

--- tests/wordwrap.lua
+++ tests/wordwrap.lua
@@ -2,4 +2,4 @@ local text = require "src/text"
 
 local phrase = "This is a test of how effective the word wrapping technique is. Please subscribe, please don't die, programming video games, FOREVER."
 
-print(text.wrap(phrase, 2))
+print(text.wrap(phrase, 8))
