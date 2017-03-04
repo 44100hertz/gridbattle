3ef7d2a3812cfbb9b84edac0a72b42a6f304ca60
sam <sam@bernie.pres>
2017-03-04 02:08:12 -0600

    actually, screw word wrapping

--- tests/wordwrap.lua
+++
@@ -1,5 +0,0 @@
-local text = require "src/text"
-
-local phrase = "This is a test of how effective the word wrapping technique is. Please subscribe, please don't die, programming video games, FOREVER."
-
-print(text.wrap(phrase, 8))
