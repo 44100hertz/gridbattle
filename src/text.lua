f8ec8e51f30303b859fa0d703018dc7f9305c107
sam <ssaammp@gmail.com>
2017-03-04 03:55:10 -0600

    newlines in print function

--- src/text.lua
+++ src/text.lua
@@ -3,7 +3,7 @@
 local fonts = {}
 
 return {
-   draw = function (font, text, x, y)
+   draw = function (font, text, ox, oy)
       if not fonts[font] then
          img = love.graphics.newImage("res/fonts/" .. font .. ".png")
          fonts[font] = {
@@ -14,20 +14,25 @@ return {
             sb = love.graphics.newSpriteBatch(img, 256, "stream"),
          }
       end
+      local x,y = ox, oy
       local f = fonts[font]
       f.sb:clear()
       for char in text:gmatch(".") do
-         -- Make letter quads as needed
-         c = string.byte(char)
-         if not f.quads[c] then
-            f.quads[c] = love.graphics.newQuad(
-               c%16*f.char_w, math.floor(c/16)*f.char_h,
-               f.char_w, f.char_h,
-               f.img:getWidth(), f.img:getHeight()
-            )
+         if char == "\n" then
+            x,y = ox, y + f.char_h
+         else
+            c = string.byte(char)
+            -- Make letter quads as needed
+            if not f.quads[c] then
+               f.quads[c] = love.graphics.newQuad(
+                  c%16*f.char_w, math.floor(c/16)*f.char_h,
+                  f.char_w, f.char_h,
+                  f.img:getWidth(), f.img:getHeight()
+               )
+            end
+            f.sb:add(f.quads[c], x, y)
+            x = x+f.char_w
          end
-         f.sb:add(f.quads[c], x, y)
-         x = x+f.char_w
       end
       love.graphics.draw(f.sb)
    end,
