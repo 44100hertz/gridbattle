dea0c7be51fadc1064e17d6fad179f1c27ea0a8f
sam <ssaammp@gmail.com>
2017-03-05 17:02:35 -0600

    nit

--- res/battle/actors/testenemy.lua
+++ res/battle/actors/testenemy.lua
@@ -12,9 +12,7 @@ return {
       if self.hp <= 0 then
          self.despawn = true
          for _ = 1,50 do
-            actors.add(
-               {x=self.x, y=self.y, z=20}, "particle"
-            )
+            actors.add({x=self.x, y=self.y, z=20}, "particle")
          end
       end
    end,
