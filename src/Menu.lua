1811a92819d6b438abf59352c9bcf4d1fe183882
sam <ssaammp@gmail.com>
2017-03-05 08:57:30 -0600

    popped out "data" subtable from menu

--- src/Menu.lua
+++ src/Menu.lua
@@ -7,7 +7,7 @@ local Menu = {
 Menu.__index = Menu
 
 function Menu:new (new_menu)
-   self = {data=new_menu}
+   self = new_menu
    setmetatable(self, Menu)
    if new_menu.bg_image then
       self.bg_image =
@@ -18,25 +18,25 @@ end
 
 function Menu:update ()
    if input.a==1 then
-      self.data[self.sel][2]() return end
+      self[self.sel][2]() return end
    if input.dd==1 then
-      self.sel = self.sel % #self.data + 1 end
+      self.sel = self.sel % #self + 1 end
    if input.du==1 then
-      self.sel = (self.sel-2) % #self.data + 1 end
+      self.sel = (self.sel-2) % #self + 1 end
 end
 
 function Menu:draw ()
    if self.bg_image then love.graphics.draw(self.bg_image) end
-   for i,v in ipairs(self.data) do
+   for i,v in ipairs(self) do
       local w = text.get_size("title", v[1])
       local x = GAME.width / 2 - w / 2
-      local y = self.data.y + i * self.data.spacing
+      local y = self.y + i * self.spacing
       if self.sel==i then
          love.graphics.setColor(255, 100, 100)
-         text.draw(self.data.font, v[1], x, y)
+         text.draw(self.font, v[1], x, y)
          love.graphics.setColor(255, 255, 255)
       else
-         text.draw(self.data.font, v[1], x, y)
+         text.draw(self.font, v[1], x, y)
       end
    end
 end
