bd64325895a711829eb34a7b2ce10a49a176f680
sam <ssaammp@gmail.com>
2017-03-05 10:17:36 -0600

    AWFUL poison drop animation :^)

--- res/chips/PoisDrop.lua
+++ res/chips/PoisDrop.lua
@@ -1,9 +1,19 @@
 local stage = require "src/battle/stage"
 
 local ent = {
+   img = "poisdrop",
+   lifespan = 60,
+   dx = 3/60,
+   z=40, dz = 1,
    start = function (self)
-      stage.apply_stat("poison", 600, _, self.x+3, self.y)
-   end
+      self.parent.state = self.parent.states.throw
+   end,
+   update = function (self)
+      self.dz = self.dz - 1/20
+      if self.time == 60 then
+         stage.apply_stat("poison", 600, self.x, self.y)
+      end
+   end,
 }
 
 return {
