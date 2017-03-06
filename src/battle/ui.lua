bd3654ca503c225d397554d66e30900d6ce4647c
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-06 16:13:01 -0600

    hp font

--- src/battle/ui.lua
+++ src/battle/ui.lua
@@ -1,11 +1,10 @@
+local text = require "src/text"
+local anim = require "src/anim"
+
 local img = love.graphics.newImage("res/battle/ui.png")
 local sb = love.graphics.newSpriteBatch(img, 40, "stream")
-local anim = require "src/anim"
 local w,h = img:getDimensions()
-local sheet = {
-   numbers = anim.sheet(0,0,8,11,10,1,w,h)[1],
-   bar = anim.sheet(0,16,8,8,3,1,w,h)[1],
-}
+local bar = anim.sheet(0,16,8,8,3,1,w,h)[1]
 local bar_width = 128
 local bar_x = GAME.width/2 - bar_width/2
 
@@ -25,21 +24,17 @@ return {
 
       sb:clear()
       if hp and hp>0 then
-         local x,y = 8,8
-         for char in tostring(math.floor(hp)):gmatch(".") do
-            sb:add(sheet.numbers[char:byte() - ("0"):byte() + 1], x, y)
-            x = x + 8
-         end
+         text.draw("visible", tostring(math.floor(hp)), 8, 8)
       end
       local x,y = bar_x, 8
       local segs = bar_width/8 - 2
-      sb:add(sheet.bar[1], x, y)
+      sb:add(bar[1], x, y)
       for _=1,segs do
          x = x + 8
-         sb:add(sheet.bar[2], x, y)
+         sb:add(bar[2], x, y)
       end
       x = x + 8
-      sb:add(sheet.bar[3], x, y)
+      sb:add(bar[3], x, y)
       love.graphics.draw(sb)
    end,
 }
