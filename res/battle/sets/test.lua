29f259fd23241581b6c3f339441af9dca827daa4
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-12 03:13:22 -0500

    fixed set

--- res/battle/sets/test.lua
+++ res/battle/sets/test.lua
@@ -2,8 +2,8 @@ return {
    stage = {
       turf = {3,3,3},
    },
-   playerpos = {x=2, y=2},
-   actors = {
+   player = {x=2, y=2},
+   enemy = {
       {x=4, y=1}, "testEnem", 1,
       {x=4, y=3}, "testEnem", 1,
       {x=6, y=1}, "testEnem", 2,
