5cb6446e9e64c8edda787203266e601963dd1f08
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-12 15:19:57 -0500

    boots inhereit damage

--- res/chips/Boots.lua
+++ res/chips/Boots.lua
@@ -3,7 +3,6 @@ local actors = require "src/battle/actors"
 local boot = {
    img="boots",
    sheet={0,0,24,16,2,1},
-   damage=40,
    collide_die=true,
    lifespan=120,
    size=0.1,
@@ -20,12 +19,12 @@ local bootspawner = {
       if self.time==10 then
          actors.add(
             {x=self.x, y=self.y, z=40, frame=1,
-             group=self.group, side=self.side},
+             group=self.group, side=self.side, damage=self.damage},
             {ent=boot})
       elseif self.time==30 then
          actors.add(
             {x=self.x, y=self.y, z=40, frame=2,
-             group=self.group, side=self.side},
+             group=self.group, side=self.side, damage=self.damage},
             {ent=boot})
       end
    end
