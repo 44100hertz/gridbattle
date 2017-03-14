e35cf0a94b2a58b4076479be84607706ebd47f4c
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-13 20:39:23 -0500

    nit

--- res/folders/test.lua
+++ res/folders/test.lua
@@ -1,6 +1,6 @@
 return {
-   {name="Boots", ltr="a", qty=4},
-   {name="Boots", ltr="b", qty=5},
+   {name="Boots",    ltr="a", qty=4},
+   {name="Boots",    ltr="b", qty=5},
    {name="Triangle", ltr="a", qty=5},
    {name="Triangle", ltr="b", qty=4},
    {name="PoisDrop", ltr="a", qty=6},
