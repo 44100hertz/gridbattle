c308c557b8a6c734f7767e8601a13b5b439842d9
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-25 13:27:31 -0500

    don't clamp depth

--- src/depthdraw.lua
+++ src/depthdraw.lua
@@ -1,9 +1,10 @@
 local depths
 local depth_step = 1
-local min_depth = -10
-local max_depth = 10
+local min_depth
+local max_depth
 
 local reset = function ()
+   min_depth, max_depth = 0,0
    depths = {}
    for _ = min_depth, max_depth, depth_step do
       table.insert(depths, {})
@@ -15,18 +16,22 @@ reset()
 return {
    add = function (drawfn, x, y, z)
       local depth = y + z / GAME.yscale
-      if depth < min_depth then depth = min_depth end
-      if depth > max_depth then depth = max_depth end
+      if depth < min_depth then min_depth = depth end
+      if depth > max_depth then max_depth = depth end
       local screen_x = GAME.xoff + GAME.xscale * x
       local screen_y = GAME.yoff + GAME.yscale * y - z
 
-      local index = math.floor((depth-min_depth) / depth_step) + 1
+      local index = math.floor(depth / depth_step) + 1
+      if not depths[index] then depths[index] = {} end
       table.insert(depths[index], {drawfn, screen_x, screen_y})
    end,
 
    draw = function ()
-      for _,depth in ipairs(depths) do
-         for _,v in ipairs(depth) do v[1](v[2], v[3]) end
+      for i = min_depth+1, max_depth+1, depth_step do
+         depth = depths[i]
+         if depth then
+            for _,v in ipairs(depth) do v[1](v[2], v[3]) end
+         end
       end
       reset()
    end,
