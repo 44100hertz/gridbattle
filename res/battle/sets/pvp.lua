b5f32c5172dc29675cb4862496c0521bdbac2d41
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-26 02:29:49 -0500

    fixed pvp set

--- res/battle/sets/pvp.lua
+++ res/battle/sets/pvp.lua
@@ -1,8 +1,11 @@
 return {
-   players = 2,
    stage = {
       turf = {3,3,3},
    },
+   left_kind = "player",
+   left = {x=2, y=2},
+   right_kind = "player",
+   right = {x=5, y=2},
    bg="scroll",
-   bg_args={"test"},
+   bg_args={"pvp"},
 }
