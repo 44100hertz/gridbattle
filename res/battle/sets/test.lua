112dcbfccfd7d0d97db863b072f849e3057608cc
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-26 00:35:58 -0500

    new set format

--- res/battle/sets/test.lua
+++ res/battle/sets/test.lua
@@ -2,7 +2,10 @@ return {
    stage = {
       turf = {3,3,3},
    },
-   enemy = {
+   left_kind = "player",
+   left = {x=2, y=2},
+   right_kind = "enemy",
+   right = {
       {name="test1", x=4, y=1},
       {name="test1", x=4, y=3},
       {name="test2", x=6, y=1},
