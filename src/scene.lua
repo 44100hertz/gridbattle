085e9a7e8f745319fe73acbbbfa65cd7bdfe2dd4
Samuel Pagenkopf <ssaammp@gmail.com>
2019-08-02 13:14:31 -0500

    cleanup src/scene

--- src/scene.lua
+++ src/scene.lua
@@ -1,18 +1,18 @@
---[[ A state is a modular piece of game runtime, such as menu, a
-battle, a dialog, etc. They handle the current inputs and drawing, and
-can be layered.
---]]
+-- a singleton that stores a stack of scenes
+-- the stack top is the current game state.
 
 local input = require "src/input"
+local fade = require 'src/scenes/fade'
 
+local scene = {}
 local stack = {}
 
-local push = function (mod, ...)
+function scene.push (mod, ...)
    table.insert(stack, mod)
    if mod.start then mod.start(...) end
 end
 
-local pop = function ()
+function scene.pop ()
    if #stack > 0 then
       local removed = table.remove(stack)
       if removed.exit then removed:exit() end
@@ -20,36 +20,32 @@ local pop = function ()
    end
 end
 
-local push_fade = function (fadeopts, mod, a,b,c,d,e)
+function scene.push_fade (fadeopts, mod, a,b,c,d,e)
    local length = fadeopts.length or 0.25
    local fadein = function ()
-      pop()
-      push(mod, a,b,c,d,e)
-      push(require"src/scenes/fade", length, true, pop)
+      scene.pop()
+      scene.push(mod, a,b,c,d,e)
+      scene.push(fade, length, true, scene.pop)
    end
-   push(require"src/scenes/fade", length, false, fadein)
+   scene.push(fade, length, false, fadein)
 end
 
-return {
-   push = push,
-   push_fade = push_fade,
-   pop = pop,
-   update = function ()
-      local top = stack[#stack]
-      if top.open and stack[#stack-1].update then stack[#stack-1]:update() end
-      local inputs = input.resolve()
-      if stack[#stack].update then stack[#stack]:update(inputs) end
-   end,
+function scene.update ()
+   local top = stack[#stack]
+   if top.open and stack[#stack-1].update then stack[#stack-1]:update() end
+   local inputs = input.resolve()
+   if stack[#stack].update then stack[#stack]:update(inputs) end
+end
 
-   draw = function ()
-      local pos = #stack
-      while(stack[pos].transparent) do
-         pos = pos - 1
-      end
-      while(stack[pos]) do
-         if stack[pos].draw then stack[pos]:draw() end
-         pos = pos + 1
-      end
-   end,
-}
+function scene.draw ()
+   local pos = #stack
+   while(stack[pos].transparent) do
+      pos = pos - 1
+   end
+   while(stack[pos]) do
+      if stack[pos].draw then stack[pos]:draw() end
+      pos = pos + 1
+   end
+end
 
+return scene
